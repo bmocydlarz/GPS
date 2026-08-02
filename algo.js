6aedd6280ad984b1f08bb6c8717a7c683f338b44
Baptiste <bmocydlvrz@icloud.com>
2026-08-02 13:53:08 +0200

    Mise en place finale avec Tailscale

--- algo.js
+++ algo.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 function calculateTollCost(route) {
